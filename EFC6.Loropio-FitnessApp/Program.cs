49d4c8c3877a2a85450ee5b46295d5eb2275fb11
Jerry Lokoroi <jerrylokoroi@gmail.com>
2023-12-10 12:53:53 +0300

    Exersice 3

--- EFC6.Loropio-FitnessApp/Program.cs
+++ EFC6.Loropio-FitnessApp/Program.cs
@@ -4,17 +4,78 @@ using Microsoft.EntityFrameworkCore;
 
 using (FitnessAppContext dbContext = new FitnessAppContext())
         {
-            dbContext.Database.EnsureCreated();
+    // Ensure the database is created
+    dbContext.Database.EnsureCreated();
 
-            // Create a User and assign RunActivities
-            CreateUserWithActivities(dbContext);
+    // Delete existing data (order matters due to foreign key relationship)
+    DeleteExistingData(dbContext);
 
-            // Display Users with their RunActivities
-            DisplayUsersWithActivities(dbContext);
-        }
+    // Add Users and RunActivities
+    AddUsersAndRunActivities(dbContext);
+
+    // Display Users with their RunActivities
+    DisplayUsersWithActivities(dbContext);
+}
 
 
-    static void CreateUserWithActivities(FitnessAppContext dbContext)
+static void DeleteExistingData(FitnessAppContext dbContext)
+{
+    dbContext.RunActivities.RemoveRange(dbContext.RunActivities);
+    dbContext.users.RemoveRange(dbContext.users);
+    dbContext.SaveChanges();
+
+    Console.WriteLine("Existing data deleted successfully!");
+}
+
+
+static void AddUsersAndRunActivities(FitnessAppContext dbContext)
+{
+    // Add User 1 with RunActivities
+    var user1 = AddUser(dbContext, "JerryFeely");
+    AddRunActivity(dbContext, user1, "Morning Run", 5.0);
+    AddRunActivity(dbContext, user1, "Evening Jog", 3.5);
+
+    // Add User 2 with RunActivities
+    var user2 = AddUser(dbContext, "Faith");
+    AddRunActivity(dbContext, user2, "Afternoon Walk", 2.0);
+    AddRunActivity(dbContext, user2, "Night Sprint", 7.0);
+
+    dbContext.SaveChanges();
+
+    Console.WriteLine("Users and RunActivities added successfully!");
+}
+
+
+static User AddUser(FitnessAppContext dbContext, string userName)
+{
+    var newUser = new User
+    {
+        UserName = userName,
+        RunActivities = new List<RunActivity>()
+    };
+
+    dbContext.users.Add(newUser);
+
+    return newUser;
+}
+
+
+
+static void AddRunActivity(FitnessAppContext dbContext, User user, string name, double distance)
+{
+    var newRunActivity = new RunActivity
+    {
+        Name = name,
+        Distance = distance,
+        User = user
+    };
+
+    user.RunActivities.Add(newRunActivity);
+    dbContext.RunActivities.Add(newRunActivity);
+}
+
+
+/*static void CreateUserWithActivities(FitnessAppContext dbContext)
     {
         // Create a new user
         var newUser = new User
@@ -31,9 +92,9 @@ using (FitnessAppContext dbContext = new FitnessAppContext())
         dbContext.SaveChanges();
 
         Console.WriteLine("User created successfully with assigned RunActivities!");
-    }
+    }*/
 
-    static void DisplayUsersWithActivities(FitnessAppContext dbContext)
+static void DisplayUsersWithActivities(FitnessAppContext dbContext)
     {
         var usersWithActivities = dbContext.users
             .Include(user => user.RunActivities)
