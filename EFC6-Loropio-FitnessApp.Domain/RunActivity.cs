a2be5bfb2fe0187523d02ca830a2e16e1f4d9d7f
Jerry Lokoroi <jerrylokoroi@gmail.com>
2023-12-10 12:07:14 +0300

    EFC6-Chapter2-Exercise1

---
+++ EFC6-Loropio-FitnessApp.Domain/RunActivity.cs
@@ -0,0 +1,13 @@
+﻿namespace EFC6_Loropio_FitnessApp.Domain
+{
+    public class RunActivity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Distance { get; set; }
+
+        // Foreign key for User
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
+}
---
+++ EFC6.Loropio-FitnessApp.Data/FitnessAppContext.cs
@@ -0,0 +1,20 @@
+﻿using EFC6_Loropio_FitnessApp.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFC6.Loropio_FitnessApp.Data
+{
+    public class FitnessAppContext : DbContext
+    {
+        public DbSet<RunActivity> RunActivities { get; set; }
+
+        public DbSet<User> users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Server = localhost\\SQLEXPRESS; Database=FitnessDb; Trusted_Connection = True;"
+                );
+
+        }
+    }
+}
