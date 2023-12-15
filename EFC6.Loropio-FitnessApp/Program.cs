0fc29b475266c9c231f35a527c5b1a9c08cb1b58
Jerry Lokoroi <jerrylokoroi@gmail.com>
2023-12-15 15:32:08 +0300

    efc6-ch3-ex4

--- EFC6.Loropio-FitnessApp/Program.cs
+++ EFC6.Loropio-FitnessApp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using EFC6.Loropio_FitnessApp.Data;
 using EFC6_Loropio_FitnessApp.Domain;
 using Microsoft.EntityFrameworkCore;
+using System.Globalization;
 
 using (FitnessAppContext dbContext = new FitnessAppContext())
         {
@@ -13,11 +14,11 @@ using (FitnessAppContext dbContext = new FitnessAppContext())
     // Add Users and RunActivities
     AddMultipleUsersWithActivities(dbContext, 15, 2);
 
-    // Display Users with their RunActivities
-    DisplayUsersWithActivities(dbContext, pageSize: 5, pageNumber: 1);
-
     // Filter Users based on specific criteria
     FilterUsersByName(dbContext, "Jerry"); // Example: Filter users with LastName containing "Doe"
+
+    // Display Users with their RunActivities, sorted by UserName in ascending order
+    DisplayUsersWithActivities(dbContext, pageSize: 5, pageNumber: 1, sortBy: "UserNameAscending");
 }
 
 
@@ -29,24 +30,6 @@ static void DeleteExistingData(FitnessAppContext dbContext)
 
     Console.WriteLine("Existing data deleted successfully!");
 }
-
-
-/*static void AddUsersAndRunActivities(FitnessAppContext dbContext)
-{
-    // Add User 1 with RunActivities
-    var user1 = AddUser(dbContext, "JerryFeely");
-    AddRunActivity(dbContext, user1, "Morning Run", 5.0);
-    AddRunActivity(dbContext, user1, "Evening Jog", 3.5);
-
-    // Add User 2 with RunActivities
-    var user2 = AddUser(dbContext, "Faith");
-    AddRunActivity(dbContext, user2, "Afternoon Walk", 2.0);
-    AddRunActivity(dbContext, user2, "Night Sprint", 7.0);
-
-    dbContext.SaveChanges();
-
-    Console.WriteLine("Users and RunActivities added successfully!");
-}*/
 
 
 static void AddMultipleUsersWithActivities(FitnessAppContext dbContext, int userCount, int activitiesPerUser)
@@ -116,17 +99,16 @@ static void AddRunActivity(FitnessAppContext dbContext, User user, string name, 
     dbContext.RunActivities.Add(newRunActivity);
 }
 
-static void DisplayUsersWithActivities(FitnessAppContext dbContext, int pageSize, int pageNumber)
+
+static void FilterUsersByName(FitnessAppContext dbContext, string filter)
 {
-    var usersWithActivities = dbContext.users
+    var filteredUsers = dbContext.users
         .Include(user => user.RunActivities)
-        .OrderBy(user => user.UserId) // Order by UserId for consistent pagination
-        .Skip((pageNumber - 1) * pageSize)
-        .Take(pageSize)
+        .Where(user => user.UserName.Contains(filter) || user.UserName.Contains(filter))
         .ToList();
 
-    Console.WriteLine($"Page {pageNumber} of Users with RunActivities (Page Size: {pageSize}):");
-    foreach (var user in usersWithActivities)
+    Console.WriteLine($"Filtered Users with LastName or FirstName containing '{filter}':");
+    foreach (var user in filteredUsers)
     {
         Console.WriteLine($"User ID: {user.UserId}, Name: {user.UserName}");
         foreach (var runActivity in user.RunActivities)
@@ -138,15 +120,31 @@ static void DisplayUsersWithActivities(FitnessAppContext dbContext, int pageSize
 }
 
 
-static void FilterUsersByName(FitnessAppContext dbContext, string filter)
+
+
+static void DisplayUsersWithActivities(FitnessAppContext dbContext, int pageSize, int pageNumber, string sortBy)
 {
-    var filteredUsers = dbContext.users
-        .Include(user => user.RunActivities)
-        .Where(user => user.UserName.Contains(filter) || user.UserName.Contains(filter))
+    var query = dbContext.users.Include(user => user.RunActivities).AsQueryable();
+
+    switch (sortBy.ToLower())
+    {
+        case "usernameascending":
+            query = query.OrderBy(user => user.UserName);
+            break;
+        case "usernamedescending":
+            query = query.OrderByDescending(user => user.UserName);
+            break;
+            // Add more sorting options as needed
+    }
+
+    var usersWithActivities = query
+        .OrderBy(user => user.UserId) // Order by UserId for consistent pagination
+        .Skip((pageNumber - 1) * pageSize)
+        .Take(pageSize)
         .ToList();
 
-    Console.WriteLine($"Filtered Users with LastName or FirstName containing '{filter}':");
-    foreach (var user in filteredUsers)
+    Console.WriteLine($"Page {pageNumber} of Users with RunActivities (Page Size: {pageSize}), Sorted by: {sortBy}:");
+    foreach (var user in usersWithActivities)
     {
         Console.WriteLine($"User ID: {user.UserId}, Name: {user.UserName}");
         foreach (var runActivity in user.RunActivities)
